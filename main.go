42ccb7caaef4594c0e386e4b3c0103bcfd364cbe
Kristian Mide <fas@mide.dk>
2019-03-12 22:49:56 +0100

    Remember to initialize environment

--- main.go
+++ main.go
@@ -1,17 +1,18 @@
 package main
 
 import (
-	"log"
-
 	"github.com/fasmide/schttp/web"
+	"github.com/spf13/viper"
 
 	"github.com/fasmide/schttp/scp"
 )
 
 func main() {
-	log.Printf("blarh")
+	viper.AutomaticEnv()
+
 	scp := scp.NewServer()
 	go scp.Listen()
+
 	web := web.Server{DB: scp}
 	web.Listen()
 }
